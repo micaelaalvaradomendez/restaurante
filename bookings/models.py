7ba8f52d2d9d7df36c4eaa43b6ba143d030f81c3
frerspamela <pfrers@tierradelfuego.gob.ar>
2025-05-23 18:44:59 -0300

    estableciendo las relaciones que pide el profesor

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,7 +6,6 @@ User = get_user_model()
 class TimeSlot(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
-    is_full = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.start} - {self.end}"
@@ -16,17 +13,26 @@ class TimeSlot(models.Model):
 class Table(models.Model):
     capacity = models.IntegerField()
     description = models.CharField(max_length=200)
-    is_reserved = models.BooleanField(default=False)
-    time_slots = models.ManyToManyField(TimeSlot)  # Mesa disponible en varias franjas
+    time_slots = models.ManyToManyField(TimeSlot, through='TableTimeSlot')
 
     def __str__(self):
         return f"Mesa #{self.id} (Capacidad: {self.capacity})"
 
+class TableTimeSlot(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    timeslot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+    is_reserved = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('table', 'timeslot')
+
+    def __str__(self):
+        return f"Mesa {self.table.id} - {self.timeslot} - Reservada: {self.is_reserved}"
+
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    date = models.DateField()
-    time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+    timeslot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
     code = models.CharField(max_length=20, unique=True)
     is_approved = models.BooleanField(default=False)
     approved_date = models.DateField(null=True, blank=True)
--- notifications/models.py
+++ notifications/models.py
@@ -4,16 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_notifications') # usuario que recibe la notificacion
     title = models.CharField(max_length=100)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_notifications')
     recipients = models.ManyToManyField(User, through='NotificationStatus', related_name='notifications')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-    
+
 class NotificationStatus(models.Model):
     notification = models.ForeignKey(Notification, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
