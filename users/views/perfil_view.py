0434ebcc930103f0637de8b392739f952e8e5784
Tayra <tayraag07@gmail.com>
2025-06-03 22:23:10 -0300

    html de perfil hecho

--- users/views/perfil_view.py
+++ users/views/perfil_view.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.timezone import now
+from bookings.models import Booking
 
+@login_required
 def perfil_view(request):
-    return render(request, 'perfil.html')
+    usuario = request.user
+    reservas = Booking.objects.filter(user=usuario).select_related('table', 'timeslot')
+
+    reservas_en_curso = reservas.filter(timeslot__end__gte=now()).order_by('timeslot__start')
+    reservas_historicas = reservas.filter(timeslot__end__lt=now()).order_by('-timeslot__start')
+
+    return render(request, 'perfil.html', {
+        'usuario': usuario,
+        'reservas_en_curso': reservas_en_curso,
+        'reservas_historicas': reservas_historicas
+    })
