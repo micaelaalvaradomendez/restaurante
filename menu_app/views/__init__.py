3bbcd6cb2acd5c25d38d6c1317e6a491f673d668
frerspamela <pfrers@tierradelfuego.gob.ar>
2025-06-07 20:17:00 -0300

    ...

--- menu_app/views/__init__.py
+++ menu_app/views/__init__.py
@@ -3,5 +3,16 @@ from .home_view import HomeView
 from .menu_list_view import MenuListView
 from .product_detail_view import ProductDetailView
 from .menu_por_categorias import menu_por_categorias
+from .order_views import agregar_al_carrito, ver_carrito, confirmar_pedido
 
-__all__ = ['HomeView', 'MenuListView', 'ProductDetailView', 'menu_por_categorias']
+
+__all__ = [
+        'HomeView', 
+        'MenuListView', 
+        'ProductDetailView', 
+        'menu_por_categorias',
+        'agregar_al_carrito',
+        'ver_carrito',
+        'confirmar_pedido',
+]
+
